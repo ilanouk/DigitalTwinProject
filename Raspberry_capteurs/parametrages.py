5c8cb6123ea8e4d7c83a5106cf6cf6cef859b9d1
ilanouk <106468200+ilanouk@users.noreply.github.com>
2022-05-29 16:38:32 +0200

    Create parametrages.py

---
+++ Raspberry_capteurs/parametrages.py
@@ -0,0 +1,13 @@
+# nom des topics
+topic_temperature = "l2g1_temperature"
+topic_humidite = "l2g1_humidite"
+topic_pression = "l2g1_pression"
+topic_coordonnees = "l2g1_coordonnees"
+topic_test = "l2g1_test"
+
+# délai d'envoi des données (en seconde)
+temps = 1
+
+# connexion au broker
+url = "broker.hivemq.com"
+port = 1883
